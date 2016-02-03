af5b611206fe270800c78b1e1faff8c0016f3d51
Andrew Benson <adbenson@cmu.edu>
2016-02-03 02:39:59 -0500

    add fib function, first try

--- program.c
+++ program.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+int fib(int n)
+{
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return 42;
+}
+
 int main()
 {
   printf("Hello world!\n");
+  printf("%d\n", fib(5));
   return 0;
 }
